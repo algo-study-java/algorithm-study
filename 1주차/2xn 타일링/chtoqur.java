6cc96d16434570f023f070b7e6d7d38cb0b6fe6f
최이서 <chtoqur@gmail.com>
2025-06-11 10:49:51 +0900

    1주차 최이서 BOJ_2xn 풀이 완료

---
+++ 1주차/2xn 타일링/chtoqur.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class chtoqur {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		if (N == 1) {
+			System.out.println(1);
+            sc.close();
+			return;
+		}
+		
+		int[] dp = new int[N + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for (int i = 3; i <= N; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+		}
+		
+		System.out.println(dp[N]);
+        sc.close();
+	}
+}
