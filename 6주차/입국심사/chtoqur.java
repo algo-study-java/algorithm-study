f73a9df719b0cb34df330b0cd810b404d7579f88
Iseo Choi <chtoqur@gmail.com>
2025-07-30 22:58:52 +0900

    6주차 최이서 PG_입국심사 풀이 완료

--- 6주차/입국심사/chtoqur.java
+++ 6주차/입국심사/chtoqur.java
@@ -1,30 +1,30 @@
 import java.util.*;
 
 class Solution {
-    public long solution(int n, int[] times) {
-        long answer = 0;
+  public long solution(int n, int[] times) {
+    long answer = 0;
 
-        Arrays.sort(times);
-        int len = times.length;
-        long lt = 0;
-        long rt = (long) n * times[len - 1];
-       
-        while (lt <= rt) {
-            long mid = (lt + rt) / 2;
-            long sum = 0;
+    Arrays.sort(times);
+    int len = times.length;
+    long lt = 0;
+    long rt = (long) n * times[len - 1];
 
-            for (int i = 0; i < len; i++) {
-                sum += mid / times[i];
-            }
+    while (lt <= rt) {
+      long mid = (lt + rt) / 2;
+      long sum = 0;
 
-            if (sum < n) {
-                lt = mid + 1;
-            } else {
-                answer = mid;
-                rt = mid - 1;
-            }
-        }
+      for (int i = 0; i < len; i++) {
+        sum += mid / times[i];
+      }
 
-        return answer;
+      if (sum < n) {
+        lt = mid + 1;
+      } else {
+        answer = mid;
+        rt = mid - 1;
+      }
     }
+
+    return answer;
+  }
 }
