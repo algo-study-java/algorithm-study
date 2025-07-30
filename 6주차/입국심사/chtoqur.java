a6816fad59c92f809b4d8161c26de9a968031979
Iseo Choi <chtoqur@gmail.com>
2025-07-30 22:58:08 +0900

    6주차 최이서 PG_입국심사  풀이 완료료

---
+++ 6주차/입국심사/chtoqur.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+
+        Arrays.sort(times);
+        int len = times.length;
+        long lt = 0;
+        long rt = (long) n * times[len - 1];
+       
+        while (lt <= rt) {
+            long mid = (lt + rt) / 2;
+            long sum = 0;
+
+            for (int i = 0; i < len; i++) {
+                sum += mid / times[i];
+            }
+
+            if (sum < n) {
+                lt = mid + 1;
+            } else {
+                answer = mid;
+                rt = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+}
