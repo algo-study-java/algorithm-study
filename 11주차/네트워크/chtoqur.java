7daf8cd9f2ed56f7d8abf718a0248eb3f4990f65
Iseo Choi <chtoqur@gmail.com>
2025-09-24 23:17:30 +0900

    11주차 최이서 PG_네트워크 풀이 완료

---
+++ 11주차/네트워크/chtoqur.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+  public int solution(int n, int[][] computers) {
+    boolean[] visited = new boolean[n];
+    int answer = 0;
+
+    for (int i = 0; i < n; i++) {
+      if (!visited[i]) {
+        answer++;
+        bfs(i, computers, visited);
+      }
+    }
+
+    return answer;
+  }
+
+  private void bfs(int start, int[][] computers, boolean[] visited) {
+    Queue<Integer> queue = new ArrayDeque<>();
+    queue.offer(start);
+    visited[start] = true;
+
+    while (!queue.isEmpty()) {
+      int node = queue.poll();
+
+      for (int i = 0; i < computers.length; i++) {
+        if (computers[node][i] == 1 && !visited[i]) {
+          visited[i] = true;
+          queue.offer(i);
+        }
+      }
+    }
+  }
+}
